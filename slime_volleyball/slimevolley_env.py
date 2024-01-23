13800b71ba992a89530b559901461a99c5a8621a
chasemcd <chasecmcdonald@gmail.com>
2024-01-23 09:45:33 -0500

    add action inversion for baseline policy

--- slime_volleyball/slimevolley_env.py
+++ slime_volleyball/slimevolley_env.py
@@ -183,6 +183,14 @@ class SlimeVolleyEnv(env.MultiAgentEnv):
         assert (int(n) == n) and (n >= 0) and (n < 6)
         return self.action_table[n]
 
+    def invert_action(action: np.ndarray | list) -> list:
+        """
+        used to invert the baseline policy action so that the human can use the correct
+        controls on both sides.
+        """
+        left, up, right = action
+        return [right, up, left]
+
     def step(self, actions):
         """
         note: although the action space is multi-binary, float vectors
@@ -196,6 +204,7 @@ class SlimeVolleyEnv(env.MultiAgentEnv):
         if left_agent_action is None:  # override baseline policy
             obs = self.game.agent_left.get_observation()
             left_agent_action = self.policy.predict(obs)
+            left_agent_action = self.invert_action(left_agent_action)
 
         self.game.agent_left.set_action(left_agent_action)
         self.game.agent_right.set_action(right_agent_action)
